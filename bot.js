b1515a7327031611142bc6527f3ac5d34eccf3a2
Asell123 <54206560+Asell123@users.noreply.github.com>
2019-09-06 22:50:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "516339279425830947") return;
+  if (message.author.id !== "562311501717831682") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
